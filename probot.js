95786d25a32ade79f71a5377bdd22531d0b00fd5
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2019-03-16 00:39:45 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -47,6 +47,54 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 
  
 
+client.on('message', async message => {
+
+    let amount = 1000000000000000000000000;
+
+    if(message.content.startsWith(prefix + "astrid")) {
+
+    if(message.author.bot) return;
+
+    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+
+    
+
+    let userData = credits[message.author.id];
+
+    let m = userData.credits + amount;
+
+    credits[message.author.id] = {
+
+    credits: m
+
+    };
+
+    fs.writeFile("./profile.json", JSON.stringify(userData.credits + amount), (err) => {
+
+    if (err) console.error(err);
+
+    });
+
+    
+
+    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+
+        coolDown.add(message.author.id);
+
+    });
+
+    
+
+    setTimeout(() => {
+
+       coolDown.remove(message.author.id);
+
+    },86400000);
+
+    }
+
+});
+	
 if(message.content.startsWith(prefix + "daily")) {
 
   if(profile[message.author.id].lastDaily != moment().format('day')) {
